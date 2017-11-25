6d8da9690df98160bc1683b2b3934b8d5da783a2
Qasim Zaib <zaib_qasim@hotmail.com>
2017-11-26 01:28:21 +0500

    Cascading dropdown in angular

--- ClientApp/app/components/vehicle-form/vehicle-form.component.ts
+++ ClientApp/app/components/vehicle-form/vehicle-form.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { MakeService } from '../../services/make.service';
 
 @Component({
-  selector: 'app-vehicle-form',
-  templateUrl: './vehicle-form.component.html',
-  styleUrls: ['./vehicle-form.component.css']
+	selector: 'app-vehicle-form',
+	templateUrl: './vehicle-form.component.html',
+	styleUrls: ['./vehicle-form.component.css']
 })
 export class VehicleFormComponent implements OnInit {
+	makes: any[];
+	models: any[];
+	vehicle: any = {};
+	constructor(private makeService: MakeService) { }
 
-  constructor() { }
+	ngOnInit() {
+		this.makeService.getMakes().subscribe(makes => this.makes = makes);
+	}
 
-  ngOnInit() {
-  }
-
+	onMakeChange() {
+		var selectedMake = this.makes.find(m => m.id == this.vehicle.make);
+		this.models = selectedMake ? selectedMake.models : [];
+	}
 }
