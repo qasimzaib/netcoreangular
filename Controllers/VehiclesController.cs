c4c9c7561676356cd80c37ca0077527e81c61ff9
Qasim Zaib <zaib_qasim@hotmail.com>
2017-11-26 11:49:11 +0500

    Mapping between vehicle and vehicleResource

--- Controllers/VehiclesController.cs
+++ Controllers/VehiclesController.cs
@@ -1,15 +1,20 @@
+using app.Controllers.Resources;
 using app.Models;
+using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 
 namespace app.Controllers {
 	[Route("/api/vehicles")]
 	public class VehiclesController : Controller {
-		public VehiclesController() {
-			
+		private readonly IMapper mapper;
+		public VehiclesController(IMapper mapper) {
+			this.mapper = mapper;
+
 		}
 
 		[HttpPost]
-		public IActionResult CreateVehicle ([FromBody] Vehicle vehicle) {
+		public IActionResult CreateVehicle([FromBody] VehicleResource vehicleResource) {
+			var vehicle = mapper.Map<VehicleResource, Vehicle>(vehicleResource);
 			return Ok(vehicle);
 		}
 	}
--- Mapping/MappingProfile.cs
+++ Mapping/MappingProfile.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using app.Controllers.Resources;
 using app.Models;
 using AutoMapper;
@@ -8,6 +9,12 @@ namespace app.Mapping {
 			CreateMap<Make, MakeResource>();
 			CreateMap<Model, ModelResource>();
 			CreateMap<Feature, FeatureResource>();
+
+			CreateMap<VehicleResource, Vehicle>()
+				.ForMember(v => v.ContactName, opt => opt.MapFrom(vr => vr.Contact.Name))
+				.ForMember(v => v.ContactEmail, opt => opt.MapFrom(vr => vr.Contact.Email))
+				.ForMember(v => v.ContactPhone, opt => opt.MapFrom(vr => vr.Contact.Phone))
+				.ForMember(v => v.Features, opt => opt.MapFrom(vr => vr.Features.Select(id => new VehicleFeature { FeatureId = id })));
 		}
 	}
 }
