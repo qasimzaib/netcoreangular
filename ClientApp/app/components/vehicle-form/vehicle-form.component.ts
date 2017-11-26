c10271d39c0b4e08aeacb65b57da5e4ea2e75cd4
Qasim Zaib <zaib_qasim@hotmail.com>
2017-11-26 21:37:46 +0500

    Form submission

--- ClientApp/app/components/vehicle-form/vehicle-form.component.ts
+++ ClientApp/app/components/vehicle-form/vehicle-form.component.ts
@@ -10,7 +10,10 @@ export class VehicleFormComponent implements OnInit {
 	makes: any[];
 	features: any[];
 	models: any[];
-	vehicle: any = {};
+	vehicle: any = {
+		features: [],
+		contact: {}
+	};
 
 	constructor(private vehicleService: VehicleService) { }
 
@@ -20,7 +23,22 @@ export class VehicleFormComponent implements OnInit {
 	}
 
 	onMakeChange() {
-		var selectedMake = this.makes.find(m => m.id == this.vehicle.make);
+		var selectedMake = this.makes.find(m => m.id == this.vehicle.makeId);
 		this.models = selectedMake ? selectedMake.models : [];
+		delete this.vehicle.modelId;
+	}
+
+	onFeatureToggle(featureId: any, $event: any) {
+		if ($event.target.checked) {
+			this.vehicle.features.push(featureId);
+		} else {
+			var index = this.vehicle.features.indexOf(featureId);
+			this.vehicle.features.splice(index, 1);
+		}
+	}
+
+	submit() {
+		this.vehicleService.create(this.vehicle)
+			.subscribe(x => console.log(x));
 	}
 }
